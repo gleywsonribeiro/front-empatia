9a90805d51667bea4fe2ff1e7432d1fd189a7ba5
Gleywson Ribeiro <gleywson@gmail.com>
2022-11-16 11:06:20 -0300

    Teste de menuBar

--- src/pages/Usuarios/index.js
+++ src/pages/Usuarios/index.js
@@ -24,17 +24,6 @@ export default function Usuarios() {
     return (
         <div className="button-demo">
             <h1>Teste</h1>
-            <div className="card">
-                <Button label="Primary" className="p-button-raised p-button-text"/>
-                <Button label="Secondary" className="p-button-raised p-button-secondary p-button-text"/>
-                <Button label="Success" className="p-button-raised p-button-success p-button-text"/>
-                <Button label="Info" className="p-button-raised p-button-info p-button-text"/>
-                <Button label="Warning" className="p-button-raised p-button-warning p-button-text"/>
-                <Button label="Help" className="p-button-raised p-button-help p-button-text"/>
-                <Button label="Danger" className="p-button-raised p-button-danger p-button-text"/>
-                <Button label="Plain" className="p-button-raised p-button-text p-button-plain"/>
-            </div>
-
             <div className="p-card-content p-card-body card">
                 <DataTable value={products} stripedRows responsiveLayout="scroll">
                     <Column field="code" header="Code"></Column>
